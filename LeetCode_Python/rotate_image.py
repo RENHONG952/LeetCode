6b59b7a073d6ad26db61ef0aa61bb7ee14690ae7
bear <bear97523@gmail.com>
2026-09-01 04:04:32 +0800

    update rotate image

--- LeetCode_Python/rotate_image.py
+++ LeetCode_Python/rotate_image.py
@@ -3,7 +3,6 @@ class Solution(object):
         for i in range(len(matrix)):
             for j in range(i + 1, len(matrix)):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
         for i in range(len(matrix)):
             matrix[i].reverse()
             
