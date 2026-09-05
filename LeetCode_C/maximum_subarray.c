942445facddfac35292ba44723d578349923aa62
bear <bear97523@gmail.com>
2026-09-05 13:47:02 +0800

    update maximum subarray

--- LeetCode_C/maximum_subarray.c
+++ LeetCode_C/maximum_subarray.c
@@ -15,7 +15,7 @@ int maxSubArray(int* nums, int numsSize) {
 }
 
 int main(){
-    int nums[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int nums[] = {-2, 1, -3, 4, -1, 2, 1, -5};
     int numsSize = sizeof(nums) / sizeof(nums[0]);
     int result = maxSubArray(nums, numsSize);
     printf("%d", result);
